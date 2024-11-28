7299aba710f50c5fb80d234eb29e474466456db9
liquid8796 <hanam.tranle.5@gmail.com>
2024-11-28 16:30:14 +0000

    2024-11-28 23:30:14 +07:00 Asia/Saigon

---
+++ src/test/java/com/example/spring_batch_kubernates/SpringBatchKubernatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_batch_kubernates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchKubernatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
